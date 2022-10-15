5782d1b99d297fa97449b6a30ec712d340d3e13b
Everton Assis Correia <evtcorreia@gmail.com>
2022-10-15 15:52:38 -0300

    Update novaSala.jsx

--- src/components/sala/nova-sala/novaSala.jsx
+++ src/components/sala/nova-sala/novaSala.jsx
@@ -12,6 +12,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+
 export default function NovaSalaComponent() {
 
     
